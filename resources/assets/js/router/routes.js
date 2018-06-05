eaea2c894ab67a2091c2bf346a54ff3ad9522cbc
陈小龙 <13480731740@qq.com>
2018-06-05 19:36:50 +0800

    laravel vue 配置

---
+++ resources/assets/js/router/routes.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router';
+
+let routes = [
+
+    {
+   		path: '/',
+   		component: require('./components/Home')
+	}
+
+];
+
+export default new VueRouter({
+    	mode: 'history',
+    	routes
+})
